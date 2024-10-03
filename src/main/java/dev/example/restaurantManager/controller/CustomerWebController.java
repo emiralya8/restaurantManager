1fd150662fede95b47583ed97e89ca3391c28421
albert <albertprofe@gmail.com>
2024-10-03 10:50:40 +0200

    webcontroller

---
+++ src/main/java/dev/example/restaurantManager/controller/CustomerWebController.java
@@ -0,0 +1,34 @@
+package dev.example.restaurantManager.controller;
+
+import dev.example.restaurantManager.model.Customer;
+import dev.example.restaurantManager.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+import java.util.List;
+
+@RequestMapping("/web")
+@Controller
+public class CustomerWebController {
+
+    @Autowired
+    CustomerRepository customerRepository;
+
+    // CRUD for customer
+    @GetMapping("/home")
+    public String home() {
+
+        List<Customer> customers = customerRepository.findAll();
+
+        //to-do: how to inject the list of customers in the view
+        //using conainer and the template will be able to access it
+        //and thymeleaf will be able to render it
+
+        return "home.html";
+    }
+
+
+}
