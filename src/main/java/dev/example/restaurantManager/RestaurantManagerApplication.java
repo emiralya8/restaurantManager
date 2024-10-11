190540710cc63a81a7903c8bb4cc12184bd06360
albert <albertprofe@gmail.com>
2024-10-11 11:32:46 +0200

    environment variables from application.properties

--- src/main/java/dev/example/restaurantManager/RestaurantManagerApplication.java
+++ src/main/java/dev/example/restaurantManager/RestaurantManagerApplication.java
@@ -5,7 +5,9 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.PropertySource;
 
+//@PropertySource("classpath:application.properties")
 @SpringBootApplication
 public class RestaurantManagerApplication {
 
--- src/main/java/dev/example/restaurantManager/controller/CustomerController.java
+++ src/main/java/dev/example/restaurantManager/controller/CustomerController.java
@@ -3,6 +3,7 @@ package dev.example.restaurantManager.controller;
 import dev.example.restaurantManager.model.Customer;
 import dev.example.restaurantManager.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,8 +15,17 @@ import java.util.List;
 @RestController
 public class CustomerController {
 
+    @Value("${endpoint.url.customers}")
+    private String endpointUrlCustomers;
+
     @Autowired
     private CustomerService customerService;
+
+    @GetMapping("/show-endpoint")
+    public String showEndpointCustomers() {
+
+        return "The customers endpoint URL is: " + endpointUrlCustomers;
+    }
 
     // manage request by ResponseEntity with all customers
     @GetMapping("/allCustomers")
