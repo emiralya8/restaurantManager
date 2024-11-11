ca8afbb5ccb263011d2487caef079103d31bd7a5
emy <e.miralles.yataco@hotmail.com>
2024-11-11 13:01:19 +0100

    add repository interface for MenuItem entity

---
+++ src/main/java/dev/example/restaurantManager/repository/MenuItemRepository.java
@@ -0,0 +1,7 @@
+package dev.example.restaurantManager.repository;
+
+import dev.example.restaurantManager.model.MenuItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MenuItemRepository extends JpaRepository<MenuItem,String> {
+}
